3d15202da643a7c40301d409b6ab63190e005818
Bijaykundu <bijay3030@gmail.com>
2020-11-08 10:18:41 +0530

    first commit

--- IdeaProjects/spring-boot-h2/src/test/java/com/spring/h2/api/controller/EmployeeControllerTest.java
+++ IdeaProjects/spring-boot-h2/src/test/java/com/spring/h2/api/controller/EmployeeControllerTest.java
@@ -1,15 +1,28 @@
 package com.spring.h2.api.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.h2.api.model.Employee;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class EmployeeControllerTest {
 
@@ -17,6 +30,7 @@ public class EmployeeControllerTest {
     @Autowired
     private WebApplicationContext context;
 
+    ObjectMapper mapper = new ObjectMapper();
 
     @BeforeTestClass
     public void setUp(){
@@ -24,7 +38,15 @@ public class EmployeeControllerTest {
     }
 
     @Test
-   public void addTest(){
-
+   public void addTest() throws Exception {
+       Employee employee = new Employee();
+       employee.setFirstName("raj");
+       employee.setLastName("kumar");
+       employee.setDept("IT");
+       String jsonRequest = mapper.writeValueAsString(employee);
+       MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/add").content(jsonRequest).contentType(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk()).andReturn();
+       String content = mvcResult.getResponse().getContentAsString();
+       Assert.assertEquals(content,"employee created successfully");
     }
 }
+
